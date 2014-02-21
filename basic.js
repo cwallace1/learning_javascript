e4c2c2f606193ca6bc8b5af4b97ab1d22903e033
Ken Stowell <kenstowell@kens-mbp-2.metaltoad.net>
2014-02-20 19:55:25 -0800

    Walked through basic js concepts.

---
+++ basic.js
@@ -0,0 +1,108 @@
+// 'use strict';
+
+// // DATA TYPES
+// //-----------
+
+// /* Strings */
+// 'string' "string"
+
+// /* Numbers */
+// 978968756
+
+// /* Boolean */
+// true, false, 0, '*' 
+
+// var k = '';
+
+// // Always false:
+// false, 0, '', null, undefined;
+
+// /* Arrays */
+// [], new Array(10), []
+
+// /* Objects */
+// // literal
+// {
+// 	'prop': propValue
+// }
+// 'key value pair'
+
+// /* Functions */
+// function foo() {
+// 	// run some code
+// }
+
+// if(k) {
+// 	// do something
+// }
+
+// // VARS
+// var foo = 'bar';
+
+// function baz(foo) {
+// 	foo = 'bang';
+
+// 	console.log(foo);
+// 	next(foo);
+// }
+
+// function next() {
+// 	foo = 'my coworker is trying to distract me'
+// 	finally(foo);
+// }
+
+// if(foo !== 'bang') {
+// 	foo = 'bang';
+// }
+
+// baz(foo);
+// console.log(foo);
+// // bar
+
+// /* PASS BY*/
+
+// // value
+// // primitives
+// strings, numbers, booleans
+
+
+// // reference
+// objects, arrays
+
+
+// var arrayFoo = ['bar', 'baz'];
+
+// function reference(arrayFoo) {
+// 	arrayFoo[2] = 'bang';
+// }
+
+// reference(arrayFoo);
+
+// console.log(arrayFoo);
+
+// var arrayFoo = ['bar', 'baz', 'bang'];
+
+
+// // OPERATORS
+// foo = 'bar';
+
+// console.log(foo);;
+
+// /* conditional */
+// ==, ===, &&, ||, !=, !==, >=, <=;
+
+// if(9 === 9 && 9 !== '9' || 9 == '9') {
+// 	// ?????
+// }
+
+// /* arithemetic */
+
+// +, -, /, *, --, ++, -=, +=
+
+// 9
+
+// 'foo'
+
+// foo += ' bar';
+
+// 'foo bar';
